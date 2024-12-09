69f58531aca520e22219a9afa55d5b74fdc01b2d
haxsen <hassaan.ali.lel@gmail.com>
2024-12-09 21:18:03 +0500

    Added external rust endpoints for on chain comms

--- app/services/rust_client.py
+++ app/services/rust_client.py
@@ -13,3 +13,42 @@ async def confirm_restake_on_chain(operation_id: int, amount: float):
     except requests.RequestException as e:
         print(f"Error communicating with Rust service: {e}")
         return None
+
+
+async def deposit(privkey: str, amount: float):
+    try:
+        response = requests.post(
+            f"{RUST_SERVICE_URL}/deposit_with_wallet",
+            json={"amount":str(amount),"privatekey":privkey}
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error communicating with Rust service: {e}")
+        return None
+
+
+async def withdraw(privkey: str, amount: float):
+    try:
+        response = requests.post(
+            f"{RUST_SERVICE_URL}/withdraw_with_wallet",
+            json={"amount":str(amount),"privatekey":privkey}
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error communicating with Rust service: {e}")
+        return None
+
+
+async def check_balance(privkey: str):
+    try:
+        response = requests.post(
+            f"{RUST_SERVICE_URL}/check_balance_with_wallet",
+            json={"privatekey":privkey}
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error communicating with Rust service: {e}")
+        return None
